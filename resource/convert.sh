8ed26b6b3d0bc6aa08168d637174ab2f09f0d6e5
zhangxi <zhangxi_1989@sina.com>
2016-07-04 17:20:29 +0800

    add sh

---
+++ resource/convert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+./ffmpeg -i v.mov v.flv
+./ffmpeg -i v.mov v.avi
+./ffmpeg -i v.mov v.h264
+./ffmpeg -i v.mov v.mp4
+./ffmpeg -i v.mov v.mkv
+./ffmpeg -i v.mov v.wmv
+./ffmpeg -i v.mov v.3gp
+./ffmpeg -i v.mov v.m4a
+./ffmpeg -i v.mov v.rm
+./ffmpeg -i v.mov v.rmvb
+
---
+++ resource/gifsicle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+./gifsicle -O3 --lossy=10 -o 10.gif o.gif
+./gifsicle -O3 --lossy=20 -o 20.gif o.gif
+./gifsicle -O3 --lossy=30 -o 30.gif o.gif
+./gifsicle -O3 --lossy=40 -o 40.gif o.gif
+./gifsicle -O3 --lossy=50 -o 50.gif o.gif
+./gifsicle -O3 --lossy=60 -o 60.gif o.gif
+./gifsicle -O3 --lossy=70 -o 70.gif o.gif
+./gifsicle -O3 --lossy=80 -o 80.gif o.gif
+./gifsicle -O3 --lossy=90 -o 90.gif o.gif
+./gifsicle -O3 --lossy=100 -o 100.gif o.gif
+./gifsicle -O3 --lossy=110 -o 110.gif o.gif
+./gifsicle -O3 --lossy=120 -o 120.gif o.gif
+./gifsicle -O3 --lossy=130 -o 130.gif o.gif
+./gifsicle -O3 --lossy=140 -o 140.gif o.gif
+./gifsicle -O3 --lossy=150 -o 150.gif o.gif
+./gifsicle -O3 --lossy=160 -o 160.gif o.gif
+./gifsicle -O3 --lossy=170 -o 170.gif o.gif
+./gifsicle -O3 --lossy=180 -o 180.gif o.gif
+./gifsicle -O3 --lossy=190 -o 190.gif o.gif
+./gifsicle -O3 --lossy=200 -o 200.gif o.gif
+
